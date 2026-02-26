324160265387f87ef53b9499f7ece5bada54ce23
achyuth1616 <achyuth1616@gmail.com>
2026-02-26 13:59:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Reg from "./Reg";
+import Home from "./Home";
+
+function App() {
+  return (
+    <>
+      <h1>I am App</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/reg" element={<Reg />} />
+          <Route path="/h" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+function Home()
+{
+    return(
+        <>
+        <h1> Hey I'm Homeee......</h1>
+        </>
+    )
+}
+export default Home
